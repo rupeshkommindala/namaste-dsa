751974808a309b17ff6e3261915308962d1366da
Rupesh Yadav <rupeshyadav@Rupeshs-MacBook-Air.local>
2025-07-02 18:26:39 +0530

    programming101

---
+++ programming101.js
@@ -0,0 +1,35 @@
+let a = 10;
+let b = 20;
+let sum = a + b;
+let multiply = a * b;
+let subtraction = a - b;
+console.log(sum);
+console.log(multiply);
+console.log(subtraction);
+
+let firstName="Rupesh";
+let lastName="K";
+let fullName=firstName+" "+lastName;
+console.log(fullName);
+
+let name="Rupesh";
+name="Abhay";
+console.log(name);
+
+let number=10;
+let string="Rupesh";
+let result=number+string;
+console.log(result);
+
+let arr=[10,20,30,40,50,true,"Rupesh"];
+console.log(arr[0]);
+console.log(arr[5]);
+
+let obj={
+    name:"Rupesh",
+    arr:[2,3,4,5],
+    bool:true,
+    firstName:"Rupesh",
+    lastName:"Kommindala"
+}
+console.log(obj.firstName+" "+obj.lastName);
