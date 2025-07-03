3b1ee1f04c91bc347f928dd3e909be3414df560c
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-03 21:48:03 +0530

    loops01

---
+++ loops01.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 10; i++) {
+  console.log("Hello World !");
+}
+
+for (let i = 0; i <= 4; i++) {
+  console.log("Hello World ! " + i);
+}
+
+for (let i = 3; i < 5; i++) {
+  console.log("Namaste");
+}
