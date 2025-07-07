8221c1c4d95aae9c1fd1ae7a4d8bee3c255bc5ac
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-07 13:14:15 +0530

    programming101

---
+++ Warmup/programming101.js
@@ -0,0 +1,32 @@
+const a = 10;
+const b = 20;
+const sum = a + b;
+const multiply = a * b;
+const diff = a - b;
+console.log(sum);
+console.log(multiply);
+console.log(diff);
+
+let firstName = "Rupesh";
+let lastName = "Kommindala";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let arr = [1, 2, 4, 6, 8, 9, 10];
+let sum2 = arr[0] + arr[3];
+console.log(sum2);
+
+let names = ["Rupesh", "Deepika", "Virat Kohli", "Anushka"];
+console.log(names[1]);
+
+let obj = {
+  name: "Rupesh",
+  age: 27,
+  city: "Hyderabad",
+  arr: [1, 2, 3, 4],
+  bool: true,
+};
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.city);
+console.log(obj.arr);
