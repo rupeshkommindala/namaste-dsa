7946afd3ddd0674db8bca144d22b6c710fe6f2ec
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-10 07:59:33 +0530

    Warmup

--- Warmup/loops02.js
+++ Warmup/loops02.js
@@ -14,3 +14,51 @@ function searchElement(arr, target) {
 let arr = [1, 4, 7, 10, 15, 19, 23];
 let result = searchElement(arr, 100);
 console.log(result);
+
+// Write a function that returns negative numbers in an Array
+
+function countNegative(array) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let array = [1, 2, -5, -4, 3, 20, 21, -59];
+let countResult = countNegative(array);
+console.log(countResult);
+
+// Write a function that returns the largest number in an array
+
+function largestNumber(nums) {
+  let largest = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > largest) {
+      largest = nums[i];
+    }
+  }
+  return largest;
+}
+
+let nums = [1, 4, 7, 9, 13, 18, 23, 49, 54];
+let res = largestNumber(nums);
+console.log(res);
+
+// Write a function that returns the smallest number in an array
+
+function largestNumber(nums2) {
+  let smallest = Infinity;
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums2[i] < smallest) {
+      smallest = nums2[i];
+    }
+  }
+  return smallest;
+}
+
+let nums2 = [1, 4, 7, 9, 13, -200, 18, 23, 49, 54, -100];
+let res2 = largestNumber(nums2);
+console.log(res2);
