c78fad45db47288e7623b728ecc1cdaa4e31efae
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-14 10:41:58 +0530

    Arrays

--- Arrays/singleNumber.js
+++ Arrays/singleNumber.js
@@ -1,7 +1,25 @@
-//Given an array of integers every element appears twice except one find that one
-let arr = [1, 2, 3, 3, 4, 2, 1];
+// Given an array of integers every element appears twice except one find that one(LC:136)
+// Optimized Approach
+let arr = [1, 2, 3, 3, 4, 4, 5, 2, 1];
 let ans = 0;
 for (let i = 0; i < arr.length; i++) {
   ans = ans ^ arr[i];
 }
 console.log(ans);
+
+// Brute Force Approach
+
+let nums = [4, 1, 2, 1, 2];
+let hash = {};
+for (let i = 0; i < nums.length; i++) {
+  if (!hash[nums[i]]) {
+    hash[nums[i]] = 1;
+  } else {
+    hash[nums[i]]++;
+  }
+}
+for (let i = 0; i < nums.length; i++) {
+  if (hash[nums[i]] === 1) {
+    console.log(nums[i]);
+  }
+}
