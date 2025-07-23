d378af50377faaaf92cd73981a057de79a628808
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-23 21:51:32 +0530

    Arrays

---
+++ Arrays/SecondLargest.js
@@ -0,0 +1,16 @@
+function secondLargest(arr) {
+  let firstLargest = -Infinity;
+  let secondLargest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > firstLargest) {
+      secondLargest = firstLargest;
+      firstLargest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+let arr = [10, 20, -5, 56, 76, 34];
+let result = secondLargest(arr);
+console.log(result);
