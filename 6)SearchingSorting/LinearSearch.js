7f15db4e2f66aed04f42ea83522875f6153fbafb
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-26 12:41:06 +0530

    Searching & Sorting

---
+++ 6)SearchingSorting/LinearSearch.js
@@ -0,0 +1,13 @@
+// Find the index of a given number if not then return -1
+function LinearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let arr = [1, 3, 6, 9, 11, 19];
+let result = LinearSearch(arr, 10);
+console.log(result);
