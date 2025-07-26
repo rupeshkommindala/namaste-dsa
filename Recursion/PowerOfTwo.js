a46147825efc185f511662280f2b114996f2df2e
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-07-26 10:02:39 +0530

    Recursion

--- Recursion/PowerOfTwo.js
+++ Recursion/PowerOfTwo.js
@@ -1,9 +1,12 @@
 // Power of Two (LC:231)
-var isPowerOfTwo = function (n) {
+function isPowerOfTwo(n) {
   if (n === 1) {
     return true;
   } else if (n < 1 || n % 2 !== 0) {
     return false;
   }
   return isPowerOfTwo(n / 2);
-};
+}
+
+let number = isPowerOfTwo(4);
+console.log(number);
