8230229c0374d02c7d9d36394cf6e881e868cb8d
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-14 23:32:51 +0530

    Strings

--- 8)Strings/LengthOfLastWord.js
+++ 8)Strings/LengthOfLastWord.js
@@ -1,7 +1,43 @@
 // Length of Last Word (LC:58)
 
 // Also Not Recommended for Brute Force Approach
+
+/*
 let s = "   fly me   to   the moon  ";
 s = s.trim();
 s = s.split(" ");
 console.log(s[s.length - 1].length);
+*/
+
+// My Solution
+
+/*let length = s.length;
+let count = 0;
+for (let i = length - 1; i >= 0; i--) {
+  if (s[i] === " " && count !== 0) {
+    break;
+  } else if (s[i] !== " ") {
+    count++;
+  }
+}
+console.log(count);
+*/
+let s = "   fly me   to   the moon  ";
+let n = s.length - 1;
+while (n >= 0) {
+  if (s[n] === " ") {
+    n--;
+  } else {
+    break;
+  }
+}
+let count = 0;
+while (n >= 0) {
+  if (s[n] !== " ") {
+    count++;
+    n--;
+  } else {
+    break;
+  }
+}
+console.log(count);
