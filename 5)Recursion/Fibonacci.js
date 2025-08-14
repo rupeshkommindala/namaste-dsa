36148fcb88f5a912abc8cda3e5ed08c5cf74a532
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-14 21:48:30 +0530

    Recursion

--- 5)Recursion/Fibonacci.js
+++ 5)Recursion/Fibonacci.js
@@ -1,13 +1,10 @@
 // Fibonacci Number using Recursion (LC:509)
 
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (n <= 1) {
+    return n;
   }
   return fib(n - 1) + fib(n - 2);
 }
 
-let result = fib(4);
-console.log(result);
+console.log(fib(8));
