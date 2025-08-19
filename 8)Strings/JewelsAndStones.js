e4bd41db4ca3db937e3ae0b6d88db922845199fb
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-19 23:12:16 +0530

    Strings

--- 8)Strings/JewelsAndStones.js
+++ 8)Strings/JewelsAndStones.js
@@ -2,6 +2,20 @@
 
 let jewels = "aA";
 let stones = "aAAbbbb";
+let jSet = new Set();
+let count = 0;
+for (let i = 0; i < jewels.length; i++) {
+  jSet.add(jewels[i]);
+}
+for (let i = 0; i < stones.length; i++) {
+  if (jSet.has(stones[i])) {
+    count++;
+  }
+}
+console.log(count);
+
+/*let jewels = "aA";
+let stones = "aAAbbbb";
 let count = 0;
 for (let i = 0; i < stones.length; i++) {
   if (jewels.includes(stones[i])) {
@@ -9,3 +23,19 @@ for (let i = 0; i < stones.length; i++) {
   }
 }
 console.log(count);
+*/
+
+/*
+let jewels = "aA";
+let stones = "aAAbbbb";
+let count = 0;
+for (let i = 0; i < stones.length; i++) {
+  for (let j = 0; j < jewels.length; j++) {
+    if (jewels[j] === stones[i]) {
+      count++;
+      break;
+    }
+  }
+}
+console.log(count);
+*/
