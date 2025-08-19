55692dfa5fe592bef48afc10e10d682cdedd8096
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-19 21:40:01 +0530

    Strings

---
+++ 8)Strings/LengthOfLastWord2.js
@@ -0,0 +1,14 @@
+// Length of Last Word (LC:58)
+
+let s = "   fly me   to   the moon  ";
+let n = s.length - 1;
+let count = 0;
+while (n >= 0) {
+  if (s[n] !== " ") {
+    count++;
+  } else if (s[n] === " " && count > 0) {
+    break;
+  }
+  n--;
+}
+console.log(count);
