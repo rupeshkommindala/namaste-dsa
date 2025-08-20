9beefd16bb2d3e32269c1acc19723c32e17350ae
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-20 12:04:26 +0530

    Strings

--- 8)Strings/SplitAStringInBalancedStrings.js
+++ 8)Strings/SplitAStringInBalancedStrings.js
@@ -1,2 +1,36 @@
 // Split a String in Balanced Strings (LC:1221)
 
+let s = "RLRRLLRLRL";
+let temp = 0;
+let count = 0;
+for (let i = 0; i < s.length; i++) {
+  if (s[i] === "R") {
+    temp++;
+  } else {
+    temp--;
+  }
+  if (temp === 0) {
+    count++;
+  }
+}
+console.log(count);
+
+/*
+let s = "RLRRLLRLRL";
+let count = 0;
+let R = 0;
+let L = 0;
+for (let i = 0; i < s.length; i++) {
+  if (s[i] === "R") {
+    R++;
+  } else {
+    L++;
+  }
+  if (R === L) {
+    count++;
+    R = 0;
+    L = 0;
+  }
+}
+console.log(count);
+*/
