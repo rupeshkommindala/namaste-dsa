106ea142e009538cade032e65e417b06b8223736
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-22 08:03:47 +0530

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Namaste DSA 🚀
 
 # Zero to Hero
+
+# 1)Introduction
+
+# 2)WarmUp
+
+# 3)TimeSpaceComplexity
+
+# 4)Arrays
+
+# 5)Recursion
+
+# 6)SearchingSorting
--- TwoPointersSlidingWindow/IndexOfFirstOccurInString.js
+++
@@ -1,5 +0,0 @@
-// Find the Index of the First Occurence in a String (LC:28)
-
-let haystack = "sadbutsad";
-let needle = "sad";
-console.log(haystack.indexOf(needle));
