2fe0cb222864b8cc519070c8587eb9880ad2cd2c
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-02 21:14:32 +0530

    Strings

--- 8)Strings/LargestOddNumberString.js
+++ 8)Strings/LargestOddNumberString.js
@@ -1,7 +1,7 @@
 // Largest Odd Number in String (LC:1903)
 
 function LargestOddNumberString(num) {
-  let n = num.length;
+  let n = num.length - 1;
   while (n >= 0) {
     if (Number(num[n]) % 2 === 1) {
       return num.substring(0, n + 1);
@@ -11,6 +11,19 @@ function LargestOddNumberString(num) {
   return "";
 }
 
-let num = "35427";
+let num = "3542";
 let result = LargestOddNumberString(num);
 console.log(result);
+
+// function LargestOddNumberString(num) {
+//   for (let i = num.length - 1; i >= 0; i--) {
+//     if (Number(num[i]) % 2 === 1) {
+//       return num.substring(0, i + 1);
+//     }
+//   }
+//   return "";
+// }
+
+// let num = "354279";
+// let result = LargestOddNumberString(num);
+// console.log(result);
