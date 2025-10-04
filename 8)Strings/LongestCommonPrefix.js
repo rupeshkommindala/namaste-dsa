9b90cb4dfcdf1881d2d4be181d6f04785727186d
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-04 16:59:40 +0530

    Strings

---
+++ 8)Strings/LongestCommonPrefix.js
@@ -0,0 +1,17 @@
+function LongestCommonPrefix(strs) {
+  let x = 0;
+  while (x < strs[0].length) {
+    let ch = strs[0][x];
+    for (let i = 1; i < strs.length; i++) {
+      if (ch !== strs[i][x] || x === strs[i].length) {
+        return strs[0].substring(0, x);
+      }
+    }
+    x++;
+  }
+  return strs[0];
+}
+
+let strs = ["flower", "flow", "flight"];
+let res = LongestCommonPrefix(strs);
+console.log(res);
