5b4a575bc68cd47c07c29be466dc4226c781118d
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-09 09:38:53 +0530

    LinkedList

--- 7)LinkedList/DesignLinkedList.js
+++ 7)LinkedList/DesignLinkedList.js
@@ -26,20 +26,31 @@ function addAtTail(val) {
 }
 
 function addAtIndex(index, val) {
-    if (index === 0) {
-        addAtHead(val);
-        return;
-    } else if (index === this.size) {
-        addAtTail(val);
-        return;
-    } else {
-        let newNode = new Node(val);
-        let curr = this.head;
-        for (let i = 0; i < index - 1; i++) {
-            curr = curr.next;
-        }
-        newNode.next = curr.next;
-        curr.next = newNode;
+  if (index === 0) {
+    addAtHead(val);
+    return;
+  } else if (index === this.size) {
+    addAtTail(val);
+    return;
+  } else {
+    let newNode = new Node(val);
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
     }
-    this.size++;
+    newNode.next = curr.next;
+    curr.next = newNode;
+  }
+  this.size++;
+}
+
+function get(index) {
+  if (index < 0 || index >= size) {
+    return -1;
+  }
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
+  }
+  return curr.val;
 }
