0f754b81c2d0efbae863747ae546d4488b2ba3d4
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-09 17:07:01 +0530

    LinkedList

---
+++ 7)LinkedList/MiddleOfLinkedList.js
@@ -0,0 +1,33 @@
+// Middle of the Linked List (LC:876)
+
+// Way-1
+function middleNode(head) {
+  let slow = head;
+  let fast = head;
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
+
+// Way-2
+function middleNode(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
+
+// Way-3
+function middleNode(head) {
+  let slow = (fast = head);
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
