ab6712049918158e356b7868d1c527983fe0602f
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-11 07:02:12 +0530

    LinkedList

---
+++ 7)LinkedList/RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,13 @@
+// Remove Duplicates from Sorted List (LC:83)
+
+function deleteDuplicates(head) {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+}
