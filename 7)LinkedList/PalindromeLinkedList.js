49b7c9d2ade661eb8e67f8564fd533c83d7d1d87
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-24 16:04:35 +0530

    LinkedList

---
+++ 7)LinkedList/PalindromeLinkedList.js
@@ -0,0 +1,30 @@
+// Palindrome Linked List (LC:234)
+
+function isPalindrome() {
+  // Find the middle element of the Linked List
+  let slow = (fast = head);
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  // Reverse the second half of the Linked List
+  let prev = null;
+  let curr = slow;
+  while (curr) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  // Checking for Palindrome
+  let firstList = head;
+  let secondList = prev;
+  while (secondList) {
+    if (firstList.val !== secondList.val) {
+      return false;
+    }
+    firstList = firstList.next;
+    secondList = secondList.next;
+  }
+  return true;
+}
