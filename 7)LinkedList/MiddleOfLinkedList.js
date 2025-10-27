42074bd6d6e1be8a7163822c0c80abc42f44b51e
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-10-27 08:02:27 +0530

    LinkedList

--- 7)LinkedList/MiddleOfLinkedList.js
+++ 7)LinkedList/MiddleOfLinkedList.js
@@ -4,7 +4,7 @@
 function middleNode(head) {
   let slow = head;
   let fast = head;
-  while (fast != null && fast.next != null) {
+  while (fast !== null && fast.next !== null) {
     slow = slow.next;
     fast = fast.next.next;
   }
