aa5d7dcf99b7a75f7841de547dbd1afedfeb2803
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-03 20:54:54 +0530

    TwoPointersSlidingWindow

--- 11)TwoPointersSlidingWindow/FindIndexOfFirstOccurenceInString.js
+++ 11)TwoPointersSlidingWindow/FindIndexOfFirstOccurenceInString.js
@@ -3,9 +3,3 @@
 let haystack = "saturdaysad";
 let needle = "sad";
 console.log(haystack.indexOf(needle));
-
-// Find the Index of the First Occurence in a String (LC:28)
-
-let haystack1 = "saturdaysad";
-let needle1 = "sad";
-console.log(haystack.indexOf(needle));
