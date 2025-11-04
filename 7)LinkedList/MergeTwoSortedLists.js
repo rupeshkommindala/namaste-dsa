111868989cc1fb716ace27a3d455325322070b25
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-04 20:32:56 +0530

    LinkedList

---
+++ 7)LinkedList/MergeTwoSortedLists.js
@@ -0,0 +1,32 @@
+// Merge Two Sorted Lists (LC:21)
+
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  let curr = null;
+  if (l1.val < l2.val) {
+    curr = l1;
+    l1 = l1.next;
+  } else {
+    curr = l2;
+    l2 = l2.next;
+  }
+  let start = curr;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+  if (!l1) {
+    curr.next = l2;
+  }
+  if (!l2) {
+    curr.next = l1;
+  }
+  return start;
+}
