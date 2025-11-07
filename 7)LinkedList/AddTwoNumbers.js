7c49e90ddb13a5a16a798f6c6c982856da266cb3
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-07 12:25:20 +0530

    LinkedList

---
+++ 7)LinkedList/AddTwoNumbers.js
@@ -0,0 +1,18 @@
+// Add Two Numbers (LC:2)
+
+function addTwoNumbers(l1, l2) {
+  let ans = new ListNode();
+  let ansHead = ans;
+  let carry = 0;
+  while (l1 || l2 || carry) {
+    let sum = (!l1 ? 0 : l1.val) + (!l2 ? 0 : l2.val) + carry;
+    carry = Math.floor(sum / 10);
+    let digit = sum % 10;
+    let newNode = new ListNode(digit);
+    ans.next = newNode;
+    ans = ans.next;
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+  }
+  return ansHead.next;
+}
