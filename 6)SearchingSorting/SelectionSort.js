539e321e4e6d71a10cffce51325378955b72fe18
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-10 15:34:18 +0530

    SearchingSorting

---
+++ 6)SearchingSorting/SelectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      let temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
+let arr = [5, 4, 3, 0, 2, 1];
+let res = selectionSort(arr);
+console.log(res);
