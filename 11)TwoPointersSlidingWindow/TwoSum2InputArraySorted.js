0c3d19ed44dedfdb1b29710ba5ceff210ecc4c16
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-13 19:50:48 +0530

    TwoPointersSlidingWindow

---
+++ 11)TwoPointersSlidingWindow/TwoSum2InputArraySorted.js
@@ -0,0 +1,16 @@
+// Two Sum II - Input Array Is Sorted (LC:167)
+
+var twoSum = function (arr, target) {
+  let i = 0;
+  let j = arr.length - 1;
+  while (i < j) {
+    let sum = arr[i] + arr[j];
+    if (sum > target) {
+      j--;
+    } else if (sum < target) {
+      i++;
+    } else {
+      return [i + 1, j + 1];
+    }
+  }
+};
