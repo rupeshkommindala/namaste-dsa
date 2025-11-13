4183ed3508dee5e282fb4c5f4beab979712ca62f
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-13 16:58:04 +0530

    BinarySearchAlgorithm

---
+++ 10)BinarySearchAlgorithm/FindFirstAndLastPosOfElemInSortedArray.js
@@ -0,0 +1,24 @@
+// Find First and Last Position of Element in Sorted Array (LC:34)
+
+function searchRange(arr, target) {
+  let l = 0;
+  let r = arr.length - 1;
+  let ans = [-1, -1];
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (arr[m] < target) l = m + 1;
+    else r = m;
+  }
+  if (arr[l] === target) ans[0] = l;
+
+  l = 0;
+  r = arr.length - 1;
+  while (l < r) {
+    let m = l + Math.ceil((r - l) / 2);
+    if (arr[m] > target) r = m - 1;
+    else l = m;
+  }
+  if (arr[l] === target) ans[1] = l;
+
+  return ans;
+}
