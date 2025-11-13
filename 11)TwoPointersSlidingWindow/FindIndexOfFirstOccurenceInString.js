f96b54c8e67a5ef063410321d60ca4e2dbcbae05
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-13 22:26:26 +0530

    TwoPointersSlidingWindow

--- 11)TwoPointersSlidingWindow/FindIndexOfFirstOccurenceInString.js
+++ 11)TwoPointersSlidingWindow/FindIndexOfFirstOccurenceInString.js
@@ -2,4 +2,22 @@
 
 let haystack = "saturdaysad";
 let needle = "sad";
-console.log(haystack.indexOf(needle));
+
+var strStr = function (haystack, needle) {
+  let n = haystack.length;
+  let m = needle.length;
+  for (let i = 0; i <= n - m; i++) {
+    let j = 0;
+    for (j = 0; j < m; j++) {
+      if (haystack[i + j] != needle[j]) {
+        break;
+      }
+    }
+    if (j === m) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(strStr(haystack, needle));
