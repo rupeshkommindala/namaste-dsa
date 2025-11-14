73e62a283aa9b6be505bf3baba6ab5b99648b25c
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-14 08:12:28 +0530

    TwoPointersSlidingWindow

---
+++ 11)TwoPointersSlidingWindow/ContainerWithMostWater.js
@@ -0,0 +1,17 @@
+// Container With Most Water (LC:11)
+
+function maxArea(arr) {
+  let i = 0;
+  let j = arr.length - 1;
+  let maxWater = 0;
+  while (i < j) {
+    let area = Math.min(arr[i], arr[j]) * (j - i);
+    maxWater = Math.max(area, maxWater);
+    if (arr[i] < arr[j]) i++;
+    else j--;
+  }
+  return maxWater;
+}
+let arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+let res = maxArea(arr);
+console.log(res);
