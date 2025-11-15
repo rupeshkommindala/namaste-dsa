4591e256cfbbf0a061cdc9c26d1409aeedf573f3
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-15 15:04:17 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/SingleElementInASortedArray.js
+++ 10)BinarySearchAlgorithm/SingleElementInASortedArray.js
@@ -17,3 +17,33 @@ var singleNonDuplicate = function (arr) {
     }
   }
 };
+
+// Optimal Approach (TC:O(logn) SC:O(1))
+
+var singleNonDuplicate = function (arr) {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    // Pair exists on left side
+    if (arr[m] === arr[m - 1]) {
+      leftCount = m - 1 - l;
+      if (leftCount % 2 === 1) {
+        r = m - 2;
+      } else {
+        l = m + 1;
+      }
+    }
+    // Pair exists on right side
+    else if (arr[m] === arr[m + 1]) {
+      leftCount = m - l;
+      if (leftCount % 2 === 1) {
+        r = m - 1;
+      } else {
+        l = m + 2;
+      }
+    } else {
+      return arr[m];
+    }
+  }
+};
