aeef8d9d50d188ff913fab2b86cd3ae6e4a53ba0
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-19 17:12:36 +0530

    WarmUp

--- 2)WarmUp/ReverseInteger.js
+++ 2)WarmUp/ReverseInteger.js
@@ -1,6 +1,7 @@
 // Reverse Integer (LC:7)
 
 function ReverseInteger(n) {
+  let ncopy = n;
   n = Math.abs(n);
   let rev = 0;
   while (n > 0) {
@@ -8,17 +9,13 @@ function ReverseInteger(n) {
     rev = 10 * rev + rem;
     n = Math.floor(n / 10);
   }
-  return rev;
+  let limit = Math.pow(2, 31);
+  if (rev < -limit || rev > limit - 1) {
+    return 0;
+  }
+  return ncopy < 0 ? -rev : rev;
 }
 
-let number = -12345;
+let number = 12345;
 let res = ReverseInteger(number);
-let limit = Math.pow(2, 31);
-if (res < -limit || res > limit - 1) {
-  console.log(0);
-}
-if (number < 0) {
-  console.log(-res);
-} else {
-  console.log(res);
-}
+console.log(res);
