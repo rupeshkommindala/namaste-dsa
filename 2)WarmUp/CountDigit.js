3549276dffac7053156b7fb8857d659e865c67a8
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-19 17:07:23 +0530

    WarmUp

--- 2)WarmUp/CountDigit.js
+++ 2)WarmUp/CountDigit.js
@@ -1,11 +1,11 @@
 // Write a function that returns the count of digits in a number
 
 function countDigit(n) {
-  // Converting Negative Number to Positive Number
-  n = Math.abs(n);
   if (n === 0) {
     return 1;
   }
+  // Converting Negative Number to a Positive Number
+  n = Math.abs(n);
   let count = 0;
   while (n > 0) {
     n = Math.floor(n / 10);
