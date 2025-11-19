5ac38e2e1c9d2f41604f1f6d7bc7beda304e9e9f
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-19 17:10:17 +0530

    WarmUp

--- 2)WarmUp/Palindrome.js
+++ 2)WarmUp/Palindrome.js
@@ -4,19 +4,19 @@ function palindrome(n) {
   if (n < 0) {
     return false;
   }
+  let nCopy = n;
   let rev = 0;
   while (n > 0) {
     rem = n % 10;
     rev = 10 * rev + rem;
     n = Math.floor(n / 10);
   }
-  return rev;
+  if (nCopy === rev) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 let num = 121;
-let rev = palindrome(num);
-if (rev === num) {
-  console.log(true);
-} else {
-  console.log(false);
-}
+let res = palindrome(num);
+console.log(res);
