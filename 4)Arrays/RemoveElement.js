5619fe653271dfab0e25d063ef3e0e0866cc1ca0
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-20 09:51:59 +0530

    Arrays

--- 4)Arrays/RemoveElement.js
+++ 4)Arrays/RemoveElement.js
@@ -1,9 +1,10 @@
 // Remove Element (LC:27)
+
 let nums = [3, 2, 1, 5, 3, 4, 8, 3];
 let val = 3;
 let x = 0;
 for (let i = 0; i < nums.length; i++) {
-  if (nums[i] != val) {
+  if (nums[i] !== val) {
     nums[x] = nums[i];
     x = x + 1;
   }
