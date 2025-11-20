8f914793bfa8136595d9fdedaad9868a621930ad
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-20 10:38:16 +0530

    Arrays

--- 4)Arrays/MaxConsecutiveOnes.js
+++ 4)Arrays/MaxConsecutiveOnes.js
@@ -1,19 +1,19 @@
 // Max Consecutive Ones (LC:485)
 
 let nums = [1, 1, 0, 1, 1, 1];
-let currentCount = 0;
+let currCount = 0;
 let maxCount = 0;
 for (let i = 0; i < nums.length; i++) {
-  if (nums[i] == 1) {
-    currentCount++;
-  } else {
-    if (currentCount > maxCount) {
-      maxCount = currentCount;
+  if (nums[i] === 1) {
+    currCount++;
+  } else if (nums[i] === 0) {
+    if (currCount > maxCount) {
+      maxCount = currCount;
     }
-    currentCount = 0;
+    currCount = 0;
   }
 }
-if (currentCount > maxCount) {
-  maxCount = currentCount;
+if (currCount > maxCount) {
+  maxCount = currCount;
 }
 console.log(maxCount);
