7cfc4ac25eb39cc28aa0a8bf680cb5a73c6447e3
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-20 10:44:20 +0530

    Arrays

--- 4)Arrays/MissingNumber.js
+++ 4)Arrays/MissingNumber.js
@@ -1,8 +1,9 @@
 // Find the Missing Number (LC:268)
 
-let nums = [3, 0, 1];
+// Optimal Approach (TC:O(n) SC:O(1))
+let nums = [3, 0, 1, 2, 5];
 let n = nums.length;
-let totalSum = (n * (n + 1)) / 2;
+let totalSum = Math.floor((n * (n + 1)) / 2);
 let calculatedSum = 0;
 for (let i = 0; i < n; i++) {
   calculatedSum = calculatedSum + nums[i];
