6c42ffe8897bb21a20f298cb6a6b83940d8d4d93
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-21 18:10:40 +0530

    WarmUp

--- 2)WarmUp/SecondLargest.js
+++ 2)WarmUp/SecondLargest.js
@@ -1,11 +1,13 @@
-// Find the second largest in an Array
+// Find the second largest number in an Array
 
 function secondLargestNumber(arr) {
-  let firstLargest = -Infinity;
-  let secondLargest = -Infinity;
   if (arr.length < 2) {
     return "Array should have atleast 2 elements";
   }
+
+  let firstLargest = -Infinity;
+  let secondLargest = -Infinity;
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > firstLargest) {
       secondLargest = firstLargest;
@@ -16,6 +18,7 @@ function secondLargestNumber(arr) {
   }
   return secondLargest;
 }
-let arr = [10,20,20];
+
+let arr = [10, 20, 20];
 let res = secondLargestNumber(arr);
 console.log(res);
