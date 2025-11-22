428898c66dcdf8b50c63a763d98a64d36007d154
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-22 07:17:27 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/SqrtX.js
+++ 10)BinarySearchAlgorithm/SqrtX.js
@@ -1,25 +1,35 @@
 // Sqrt(x) (LC:69)
 
 let x = 100;
+
 // Way-01 (Not Recommended)
 console.log(Math.trunc(Math.pow(x, 0.5)));
+
 // Way-02 (Not Recommended)
 console.log(Math.trunc(x ** 0.5));
-// Way-03
 
-if(x<2){
+// Way-03 (TC:O(logn) Recommended)
+function sqrt(x) {
+  if (x < 2) {
     return x;
-}
-let left = 2;
-let right = Math.floor(x / 2);
-while (left <= right) {
-  let mid = Math.floor((left + right) / 2);
-  if (mid * mid === x) {
-    return mid;
-  } else if (mid * mid > x) {
-    right = mid - 1;
-  } else {
-    left = mid + 1;
   }
+  let l = 2;
+  let r = Math.floor(x / 2);
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2);
+    if (m * m === x) {
+      return m;
+    } else if (m * m < x) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  return r;
 }
-return Math.floor(right);
+
+let num = 100;
+let res = sqrt(num);
+console.log(res);
+
+// Way-04 (Using Linear Search TC:O(n) HW)
