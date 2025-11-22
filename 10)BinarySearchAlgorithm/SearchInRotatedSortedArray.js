a351e63d057b09e8d9680b1808087dc0a452b342
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-22 09:36:05 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/SearchInRotatedSortedArray.js
+++ 10)BinarySearchAlgorithm/SearchInRotatedSortedArray.js
@@ -1,25 +1,31 @@
 // Search in Rotated Sorted Array (LC:33)
 
-let l = 0;
-let r = arr.length - 1;
-while (l <= r) {
-  let m = l + Math.floor((r - l) / 2);
-  if (arr[m] === target) {
-    return m;
-  }
-  // Left part is Sorted
-  if (arr[l] <= arr[m]) {
-    if (target >= arr[l] && target < arr[m]) {
-      r = m - 1;
-    } else {
-      l = m + 1;
+function search(arr, target) {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (arr[m] === target) {
+      return m;
     }
-  } else {
-    if (target > arr[m] && target <= arr[r]) {
-      l = m + 1;
+    // Left part is Sorted
+    if (arr[l] <= arr[m]) {
+      if (target >= arr[l] && target < arr[m]) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
     } else {
-      r = m - 1;
+      if (target > arr[m] && target <= arr[r]) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
     }
   }
+  return -1;
 }
-return -1;
+
+let arr = [4, 5, 6, 7, 0, 1, 2];
+let res = search(arr, 0);
+console.log(res);
