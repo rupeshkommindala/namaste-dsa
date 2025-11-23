d7e59d8546f919c0a997119c81beb89fa026b1fd
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-23 06:09:44 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/PeakIndexInAMountainArray.js
+++ 10)BinarySearchAlgorithm/PeakIndexInAMountainArray.js
@@ -1,16 +1,21 @@
 // Peak Index in a Mountain Array (LC:852)
 
-function peakIndexInMountainArray(arr){
-    let l=0;
-    let r=arr.length-1;
-    while(l<r){
-        let m=l+Math.floor((r-l)/2);
-        if(arr[m+1]>arr[m]){
-            l=m+1;
-        }
-        else{
-            r=m;
-        }
+function peakIndexInMountainArray(arr) {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (arr[m + 1] > arr[m]) {
+      l = m + 1;
+    } else {
+      r = m;
     }
-    return l;
+  }
+  return l; // return l or r
 }
+
+let arr = [0, 1, 0];
+let res = peakIndexInMountainArray(arr);
+console.log(res);
+
+// Brute Force Approach (HW)
