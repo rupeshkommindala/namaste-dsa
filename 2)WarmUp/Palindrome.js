05cdbc0da87fa83530074df49f07d09178cf56f0
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-12-08 21:27:54 +0530

    WarmUp

--- 2)WarmUp/Palindrome.js
+++ 2)WarmUp/Palindrome.js
@@ -5,8 +5,8 @@ function palindrome(x) {
   let xCopy = x;
   let rev = 0;
   while (x > 0) {
-    let rem = x % 10;
-    rev = 10 * rev + rem;
+    let last = x % 10;
+    rev = 10 * rev + last;
     x = Math.floor(x / 10);
   }
   return xCopy === rev;
