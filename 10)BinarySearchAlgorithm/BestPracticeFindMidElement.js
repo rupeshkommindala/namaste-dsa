dcea9b67d4a0117b49397240e4499cbe0138a63a
Rupesh  K <rupeshkommindala@gmail.com>
2025-12-13 19:44:48 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/BestPracticeFindMidElement.js
+++ 10)BinarySearchAlgorithm/BestPracticeFindMidElement.js
@@ -1,9 +1,18 @@
-// It can lead to Overflow;
+/**
+ * This works perfectly fine in JavaScript, but in other
+ * languages it will create a lot of issues.
+ * It can lead to overflow in other languages not JavaScript
+ */
+
 let l = 2;
 let r = 6;
-let m = Math.floor((l + r) / 2);
+let m = Math.floor((l + r) / 2); // Big Integer + Big Integer can lead to overflow
 console.log(m);
 
-// Never Overflow;
-let mid = l + Math.floor((r - l) / 2);
+/**
+ * This works perfectly fine in all the languages.
+ * Never overflow
+ */
+
+let mid = l + Math.floor((r - l) / 2); // Big Integer - Big Integer never overflow
 console.log(mid);
