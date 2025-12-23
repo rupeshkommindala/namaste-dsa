84d3ef8560bd1f944c0415327043e0bf9c50b2d9
Rupesh  K <rupeshkommindala@gmail.com>
2025-12-23 18:45:24 +0530

    Arrays

--- 4)Arrays/RemoveDuplicates.js
+++ 4)Arrays/RemoveDuplicates.js
@@ -1,12 +1,16 @@
 // Remove Duplicates from Sorted Array (LC:26)
 
-let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-let x = 0;
-for (let i = 0; i < nums.length; i++) {
-  if (nums[i] > nums[x]) {
-    x = x + 1;
-    nums[x] = nums[i];
+var removeDuplicates = function (nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
+    }
   }
-}
-console.log(nums);
-console.log(x + 1);
+  return x + 1;
+};
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+let res = removeDuplicates(nums);
+console.log(res);
