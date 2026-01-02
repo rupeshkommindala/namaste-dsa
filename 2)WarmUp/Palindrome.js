cd36839f50e59e000a915c3e2d12138595baa9cb
Rupesh  K <rupeshkommindala@gmail.com>
2026-01-02 18:45:56 +0530

    WarmUp

--- 2)WarmUp/Palindrome.js
+++ 2)WarmUp/Palindrome.js
@@ -1,21 +1,22 @@
 // Palindrome Number (LC:9)
 
-function palindrome(x) {
-  if (x < 0) return false;
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
   let xCopy = x;
   let rev = 0;
   while (x > 0) {
-    let last = x % 10;
-    rev = 10 * rev + last;
+    let rem = x % 10;
+    rev = 10 * rev + rem;
     x = Math.floor(x / 10);
   }
-  if (xCopy === rev) {
+  if (rev === xCopy) {
     return true;
   } else {
     return false;
   }
-  //return xCopy === rev;
-}
-let num = 12215;
-let res = palindrome(num);
+};
+let num = 1221;
+let res = isPalindrome(num);
 console.log(res);
