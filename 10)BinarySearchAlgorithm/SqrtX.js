6330447a548603ca1532a86ad164c5bfd89897e8
Rupesh  K <rupeshkommindala@gmail.com>
2026-01-03 08:53:41 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/SqrtX.js
+++ 10)BinarySearchAlgorithm/SqrtX.js
@@ -3,20 +3,20 @@
 let x = 100;
 
 // Way-01 (Not Recommended)
-console.log(Math.trunc(Math.pow(x, 0.5)));
+console.log(Math.floor(Math.pow(x, 0.5)));
 
 // Way-02 (Not Recommended)
 console.log(Math.trunc(x ** 0.5));
 
 // Way-03 (TC:O(logn) Recommended)
-function sqrt(x) {
+var mySqrt = function (x) {
   if (x < 2) {
     return x;
   }
   let l = 2;
   let r = Math.floor(x / 2);
   while (l <= r) {
-    let m = Math.floor((l + r) / 2); // let m = l + Math.floor((r - l) / 2);
+    let m = l + Math.floor((r - l) / 2); // let m = l + Math.floor((r - l) / 2);
     if (m * m === x) {
       return m;
     } else if (m * m < x) {
@@ -26,10 +26,10 @@ function sqrt(x) {
     }
   }
   return r;
-}
+};
 
 let n = 100;
-let res = sqrt(n);
+let res = mySqrt(n);
 console.log(res);
 
 // Way-04 (Using Linear Search TC:O(n) HW)
