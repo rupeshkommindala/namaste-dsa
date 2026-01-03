9419a259b86e918e8ab6a59d6e81dce52296dca8
Rupesh  K <rupeshkommindala@gmail.com>
2026-01-03 09:09:19 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/GuessNumberHigherOrLower.js
+++ 10)BinarySearchAlgorithm/GuessNumberHigherOrLower.js
@@ -1,19 +1,17 @@
 // Guess Number Higher or Lower (LC:374)
 
 var guessNumber = function (n) {
-    let l=1;
-    let r=n;
-    while(l<=r){
-        let m=l+Math.floor((r-l)/2);
-        let res=guess(m);
-        if(res===0){
-            return m;
-        }
-        else if(res<0){
-            r=m-1;
-        }
-        else{
-            l=m+1;
-        }
+  let l = 1;
+  let r = n;
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    let res = guess(m);
+    if (res === 0) {
+      return m;
+    } else if (res < 0) {
+      r = m - 1;
+    } else {
+      l = m + 1;
     }
+  }
 };
