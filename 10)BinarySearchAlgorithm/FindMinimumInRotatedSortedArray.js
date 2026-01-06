6b88c119581c49de80cbca290571682e08c2bb53
Rupesh  K <rupeshkommindala@gmail.com>
2026-01-06 12:02:07 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/FindMinimumInRotatedSortedArray.js
+++ 10)BinarySearchAlgorithm/FindMinimumInRotatedSortedArray.js
@@ -4,18 +4,11 @@ var findMin = function (arr) {
   let l = 0;
   let r = arr.length - 1;
   while (l <= r) {
-    if (arr[l] <= arr[r]) {
-      return arr[l];
-    }
+    if (arr[l] <= arr[r]) return arr[l];
     let m = l + Math.floor((r - l) / 2);
-    if (arr[m] < arr[m - 1]) {
-      return arr[m];
-    }
-    if (arr[l] <= arr[m]) {
-      l = m + 1;
-    } else {
-      r = m - 1;
-    }
+    if (arr[m] < arr[m - 1]) return arr[m];
+    if (arr[l] <= arr[m]) l = m + 1;
+    else r = m - 1;
   }
 };
 
