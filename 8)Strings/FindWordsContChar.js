7db4dfeaf06c9c26de3faf4b7238e0562e6bec7f
Rupesh  K <rupeshkommindala@gmail.com>
2026-01-09 08:59:06 +0530

    Strings

--- 8)Strings/FindWordsContChar.js
+++ 8)Strings/FindWordsContChar.js
@@ -1,25 +1,33 @@
 // Find Words Containing Character (LC:2942)
 
-let words = ["leet", "code"];
-let x = "e";
-let res = [];
-for (let i = 0; i < words.length; i++) {
-  if (words[i].includes(x)) {
-    res.push(i);
-  }
-}
-console.log(res);
-
-/*let words = ["leet", "code"];
-let x = "e";
-let res = [];
-for (let i = 0; i < words.length; i++) {
-  for(let j=0;j<words[i].length;j++){
-    if(words[i][j]===x){
-        res.push(i);
-        break;    
+// Optimized Solution (TC: O(n*m), SC:O(1)) m->length of words n->max length of each word
+var findWordsContaining = function (words, x) {
+  let ans = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === x) {
+        ans.push(i);
+        break;
+      }
     }
   }
-}
+  return ans;
+};
+
+let result = findWordsContaining(["abc", "bcd", "aaaa", "cbc"], "a");
+console.log(result);
+
+// Not Recommended (Don't use built in method(includes))
+var findWordsContainingChar = function (words, x) {
+  let ans = [];
+  for (let i = 0; i < words.length; i++) {
+    // Check if char x is present in words[i]
+    if (words[i].includes(x)) {
+      ans.push(i);
+    }
+  }
+  return ans;
+};
+
+let res = findWordsContainingChar(["leet", "code"], "e");
 console.log(res);
-*/
