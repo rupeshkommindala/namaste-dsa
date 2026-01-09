4796070ae9ddcd0ebcbabf8a7e689636898a322a
Rupesh  K <rupeshkommindala@gmail.com>
2026-01-09 07:08:04 +0530

    Strings

--- 8)Strings/LengthOfLastWord1.js
+++ 8)Strings/LengthOfLastWord1.js
@@ -1,64 +1,57 @@
-// Length of Last Word (LC:58)
+// Brute Force Solution (TC: O(n), SC:O(n))
+var lengthOfLastWord1 = function (s) {
+  s = s.trim();
+  s = s.split(" ");
+  return s[s.length - 1].length;
+};
 
-// Also Not Recommended for Brute Force Approach
+// Optimized Solution (Way-01) (TC: O(n), SC:O(1))
+var lengthOfLastWord2 = function (s) {
+  let n = s.length - 1;
+  let count = 0;
+  while (n >= 0) {
+    if (s[n] === " ") {
+      n--;
+    } else {
+      break;
+    }
+  }
+  while (n >= 0) {
+    if (s[n] !== " ") {
+      count++;
+      n--;
+    } else {
+      break;
+    }
+  }
+  return count;
+};
 
-/*
-let s = "   fly me   to   the moon  ";
-s = s.trim();
-s = s.split(" ");
-console.log(s[s.length - 1].length);
-*/
+// Optimized Solution (Way-01) (TC: O(n), SC:O(1))
+var lengthOfLastWord3 = function (s) {
+  let n = s.length - 1;
+  let count = 0;
+  while (n >= 0) {
+    if (s[n] !== " ") {
+      break;
+    } else {
+      n--;
+    }
+  }
+  while (n >= 0) {
+    if (s[n] === " ") {
+      break;
+    } else {
+      count++;
+      n--;
+    }
+  }
+  return count;
+};
 
-// My Solution
-
-/*let length = s.length;
-let count = 0;
-for (let i = length - 1; i >= 0; i--) {
-  if (s[i] === " " && count !== 0) {
-    break;
-  } else if (s[i] !== " ") {
-    count++;
-  }
-}
-console.log(count);
-*/
-let s = "   fly me   to   the moon  ";
-let n = s.length - 1;
-while (n >= 0) {
-  if (s[n] === " ") {
-    n--;
-  } else {
-    break;
-  }
-}
-let count = 0;
-while (n >= 0) {
-  if (s[n] !== " ") {
-    count++;
-    n--;
-  } else {
-    break;
-  }
-}
-console.log(count);
-
-/*let s = "   fly me   to   the moon  ";
-let n = s.length - 1;
-while (n >= 0) {
-  if (s[n] !== " ") {
-    break;
-  } else {
-    count++;
-    n--;
-  }
-}
-let count = 0;
-while (n >= 0) {
-  if (s[n] === " ") {
-    break;
-  } else {
-    n--;
-    count++;
-  }
-}
-console.log(count);*/
+let res1 = lengthOfLastWord1("Hello World");
+console.log(res1);
+let res2 = lengthOfLastWord2("   fly me   to   the moon  ");
+console.log(res2);
+let res3 = lengthOfLastWord3("luffy is still joyboy");
+console.log(res3);
