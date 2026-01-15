88d4f1c48f104a2bb93300c0b7a51fb18f96636b
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-15 20:22:49 +0530

    Arrays

--- 4)Arrays/BuyAndSellStocks.js
+++ 4)Arrays/BuyAndSellStocks.js
@@ -1,28 +1,37 @@
 // Best Time to Buy and Sell Stock (LC:121)
 
-// Brute Force Approach
-let nums = [7, 1, 5, 3, 6, 4];
-let maxProfit = 0;
-for (let i = 0; i < nums.length - 1; i++) {
-  for (let j = i + 1; j < nums.length; j++) {
-    if (nums[j] - nums[i] > maxProfit) {
-      maxProfit = nums[j] - nums[i];
+// Brute Force Solution (TC:O(n^2), SC:O(1))
+var maxProfit1 = function (prices) {
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] - prices[i] > maxProfit) {
+        maxProfit = prices[j] - prices[i];
+      }
     }
   }
-}
-console.log(maxProfit);
+  return maxProfit;
+};
 
-// Optimized Approach
-// We will keep track of minimum value that we have seen so far and the maximum profit that we get so far
-let prices = [7, 1, 5, 3, 6, 4];
-let min = prices[0];
-let profit = 0;
-for (let i = 1; i < prices.length; i++) {
-  if (prices[i] - min > profit) {
-    profit = prices[i] - min;
+let prices1 = [7, 1, 5, 3, 6, 4];
+let res1 = maxProfit1(prices1);
+console.log(res1);
+
+// Optimized Solution (TC:O(n), SC:O(1))
+var maxProfit2 = function (prices) {
+  let maxProfit = 0;
+  let min = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
+    }
+    if (prices[i] < min) {
+      min = prices[i];
+    }
   }
-  if (prices[i] < min) {
-    min = prices[i];
-  }
-}
-console.log(profit);
+  return maxProfit;
+};
+
+let prices2 = [1, 5, 8, 4, 9, 10];
+let res2 = maxProfit2(prices2);
+console.log(res2);
