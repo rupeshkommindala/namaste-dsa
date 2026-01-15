f517c91f9ae0dc18c01c4a3f7dd22d3bf4d5924f
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-15 20:29:22 +0530

    Arrays

--- 4)Arrays/MaxConsecutiveOnes.js
+++ 4)Arrays/MaxConsecutiveOnes.js
@@ -1,22 +1,18 @@
 // Max Consecutive Ones (LC:485)
 
+// Optimized Solution (TC:O(n), SC:O(1))
 var findMaxConsecutiveOnes = function (nums) {
   let currCount = 0;
   let maxCount = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 1) {
       currCount++;
-    } else {
-      if (currCount > maxCount) {
-        maxCount = currCount;
-      }
+    } else if (nums[i] === 0) {
+      maxCount = Math.max(currCount, maxCount);
       currCount = 0;
     }
   }
-  if (currCount > maxCount) {
-    return currCount;
-  }
-  return maxCount;
+  return Math.max(currCount, maxCount);
 };
 
 let nums = [1, 1, 0, 1, 1, 1];
