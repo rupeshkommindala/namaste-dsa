4e3949aafe3910a374186bb8ad8406cb5e501f89
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-15 10:11:30 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/FindPeakElement.js
+++ 10)BinarySearchAlgorithm/FindPeakElement.js
@@ -1,7 +1,24 @@
 // Find Peak Element (LC:162)
 
+// Brute Force Solution (TC:O(n), SC:O(1))
+var findPeakElement1 = function (arr) {
+  let maximum = -Infinity;
+  let index = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maximum) {
+      maximum = arr[i];
+      index = i;
+    }
+  }
+  return index;
+};
+
+let arr1 = [1, 2, 3, 1];
+let res1 = findPeakElement1(arr1);
+console.log(res1);
+
 // Optimized Solution (TC:O(logn), SC:O(1))
-var findPeakElement = function (arr) {
+var findPeakElement2 = function (arr) {
   let l = 0;
   let r = arr.length - 1;
   while (l < r) {
@@ -15,6 +32,6 @@ var findPeakElement = function (arr) {
   return r; // return l also works
 };
 
-let arr = [1, 2, 1, 3, 5, 6, 4];
-let res = findPeakElement(arr);
-console.log(res);
+let arr2 = [1, 2, 1, 3, 5, 6, 4];
+let res2 = findPeakElement2(arr2);
+console.log(res2);
