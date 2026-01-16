bb4fe5f79153f7727d2194bf0b9881071e8c4cdb
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-16 08:01:15 +0530

    Searching&Sorting

--- 6)SearchingSorting/LinearSearch.js
+++ 6)SearchingSorting/LinearSearch.js
@@ -3,6 +3,7 @@
   if the element is not present then just return -1 using Linear Search.
 */
 
+// Optimized Solution (TC:O(n), SC:O(1))
 var linearSearch = function (arr, target) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === target) {
