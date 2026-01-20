add57ca3584025a46cdb952a2882d0c864cd162f
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-20 11:03:25 +0530

    Strings

--- 8)Strings/ValidAnagram.js
+++ 8)Strings/ValidAnagram.js
@@ -1,24 +1,36 @@
-let s = "rat";
-let t = "car";
+// Valid Anagram (LC:242)
 
-if (s.length !== t.length) {
-  console.log(false);
-  return;
-}
-let map = {};
-for (let i = 0; i < s.length; i++) {
-  if (!map[s[i]]) {
-    map[s[i]] = 1;
-  } else {
-    map[s[i]]++;
+// Brute Force Solution (TC:O(logn), SC:O(n)) Not Recommended (Don't use built in methods)
+var isAnagram1 = function (s, t) {
+  return s.split("").sort().join("") === t.split("").sort().join("");
+};
+
+console.log(isAnagram1("rat", "car"));
+
+// Optimized Solution (TC:O(n), SC:O(1))
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
   }
-}
-for (let i = 0; i < t.length; i++) {
-  if (!map[t[i]] || map[t[i]] < 0) {
-    console.log(false);
-    return;
-  } else {
-    map[t[i]]--;
+  let map = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!map[s[i]]) {
+      map[s[i]] = 1;
+    } else {
+      map[s[i]]++;
+    }
   }
-}
-console.log(true);
+  for (let i = 0; i < t.length; i++) {
+    if (!map[t[i]] || map[t[i]] < 0) {
+      return false;
+    } else {
+      map[t[i]]--;
+    }
+  }
+  return true;
+};
+
+let s = "anagram";
+let t = "nagaram";
+let res = isAnagram(s, t);
+console.log(res);
