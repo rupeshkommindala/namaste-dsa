87fe49768d8f937a00880a046d5e3855a936bd37
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-20 15:18:32 +0530

    Strings

--- 8)Strings/SplitAStringInBalancedStrings.js
+++ 8)Strings/SplitAStringInBalancedStrings.js
@@ -1,6 +1,6 @@
 // Split a String in Balanced Strings (LC:1221)
 
-// Way-01 TC:O(n), SC:O(1)
+// Way-01 (TC:O(n), SC:O(1))
 var balancedStringSplit = function (s) {
   let L = 0;
   let R = 0;
@@ -24,7 +24,7 @@ let s = "RLRRLLRLRL";
 let res1 = balancedStringSplit(s);
 console.log(res1);
 
-// Way-02 TC:O(n), SC:O(1)
+// Way-02 (TC:O(n), SC:O(1))
 var balancedStringSplit2 = function (s) {
   let count = 0;
   let temp = 0;
