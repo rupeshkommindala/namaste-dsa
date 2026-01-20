0e37da0b1bcd64f9ff752cf970d57924709b4fff
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-20 09:58:31 +0530

    Recursion

--- 5)Recursion/SumOfAllOddNumbers.js
+++ 5)Recursion/SumOfAllOddNumbers.js
@@ -1,34 +1,13 @@
-// Sum of all Odd numbers in an array using Recursion
+// Sum of all Odd numbers in an Array using Recursion
 
-function sumOfOdd(n) {
-  let isOdd = arr[n] % 2 === 1;
-  if (n === 0) {
-    if (isOdd) {
-      return arr[n];
-    } else {
-      return 0;
-    }
-  }
-  if (isOdd) {
-    return arr[n] + sumOfOdd(n - 1);
-  } else {
-    return 0 + sumOfOdd(n - 1);
-  }
-}
-
-let arr = [5, 0, 3, 4, 2, 6, 1];
-let result = sumOfOdd(arr.length - 1);
-console.log(result);
-
-/*function sumOfOdd(n) {
-  let isOdd = arr[n] % 2 === 1;
+var sum = function (n) {
+  let isOdd = arr[n] % 2 == 1;
   if (n === 0) {
     return isOdd ? arr[n] : 0;
   }
+  return (isOdd ? arr[n] : 0) + sum(n - 1);
+};
 
-  return (isOdd ? arr[n] : 0) + sumOfOdd(n - 1);
-}
-
-let arr = [5, 0, 3, 4, 2, 6, 1];
-let result = sumOfOdd(arr.length - 1);
-console.log(result);*/
+let arr = [5, 3, 2, 0, 1];
+let res = sum(arr.length - 1);
+console.log(res);
