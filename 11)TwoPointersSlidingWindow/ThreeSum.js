2eca8b007a75785d7de113f0c66b33df11626c97
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-31 21:23:14 +0530

    TwoPointersSlidingWindow

--- 11)TwoPointersSlidingWindow/ThreeSum.js
+++ 11)TwoPointersSlidingWindow/ThreeSum.js
@@ -1,30 +1,34 @@
-var threeSum = function (nums) {
-  nums.sort((a, b) => a - b);
-  let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (i === 0 || nums[i] !== nums[i - 1]) {
-      twoSum(nums, i, ans);
-    }
-  }
-  return ans;
-};
-var twoSum = function (arr, x, ans) {
-  let i = x + 1;
-  let j = arr.length - 1;
-  while (i < j) {
-    let sum = arr[i] + arr[j] + arr[x];
-    if (sum > 0) {
-      j--;
-    } else if (sum < 0) {
-      i++;
-    } else {
-      ans.push([arr[i], arr[j], arr[x]]);
-      i++;
-      j--;
-      // Skip duplicates for the second element
-      while (i < j && arr[i] === arr[i - 1]) {
-        i++;
+// Two Sum (LC:1)
+
+// Brute Force Solution (TC:O(n^2), SC:O(1))
+var twoSum1 = function (arr, target) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
       }
     }
   }
 };
+
+let nums1 = [1, 3, 4, 2];
+let res1 = twoSum1(nums1, 6);
+console.log(res1);
+
+// Optimized Solution (TC:O(n), SC:O(n))
+var twoSum2 = function (arr, target) {
+  let map = {};
+  for (let i = 0; i < arr.length; i++) {
+    map[arr[i]] = i;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let pairToFind = target - arr[i];
+    if (map[pairToFind] && map[pairToFind] != i) {
+      return [i, map[pairToFind]];
+    }
+  }
+};
+
+let nums2 = [3, 2, 4];
+let res2 = twoSum2(nums2, 6);
+console.log(res2);
