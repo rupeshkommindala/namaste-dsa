c6417dbbb79bdf4735037ea61829c1457d5abbd0
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-02 08:32:13 +0530

    WarmUp

--- 2)WarmUp/CountDigit.js
+++ 2)WarmUp/CountDigit.js
@@ -1,11 +1,11 @@
 // Write a function that returns the count of digits in a number
 
+// (TC:O(d) where d = number of digits, SC:O(1))
 var countDigit = function (n) {
+  n = Math.abs(n);
   if (n === 0) {
     return 1;
   }
-  // Converting Negative Number to a Positive Number
-  n = Math.abs(n);
   let count = 0;
   while (n > 0) {
     n = Math.floor(n / 10);
@@ -14,6 +14,6 @@ var countDigit = function (n) {
   return count;
 };
 
-let num = 425;
+let num = 123;
 let res = countDigit(num);
 console.log(res);
