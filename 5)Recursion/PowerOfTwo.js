069b57ff261709cf9562bb378b3897697b65e42c
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-02 19:54:39 +0530

    Recursion

--- 5)Recursion/PowerOfTwo.js
+++ 5)Recursion/PowerOfTwo.js
@@ -1,9 +1,10 @@
 // Power of Two (LC:231)
 
+// (TC:O(logn), SC:O(logn) Recursion stack)
 var isPowerOfTwo = function (n) {
   if (n === 1) {
     return true;
-  } else if (n < 1 || n % 2 === 1) {
+  } else if (n % 2 === 1 || n < 1) {
     return false;
   }
   return isPowerOfTwo(n / 2);
