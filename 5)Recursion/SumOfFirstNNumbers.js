ec01b6a1b32bdb5899c7953d97bc38791134ca37
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-02 21:04:50 +0530

    Recursion

--- 5)Recursion/SumOfFirstNNumbers.js
+++ 5)Recursion/SumOfFirstNNumbers.js
@@ -1,5 +1,6 @@
 // Sum of First N Numbers using Recursion
 
+// (TC:O(n), SC:O(n) Recursion stack)
 var sum = function (n) {
   if (n === 0) {
     return 0;
@@ -7,6 +8,6 @@ var sum = function (n) {
   return n + sum(n - 1);
 };
 
-let num = 6;
+let num = 5;
 let res = sum(num);
 console.log(res);
