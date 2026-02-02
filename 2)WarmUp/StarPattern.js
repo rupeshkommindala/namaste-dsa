1ed96b1cda842f22d98124f958ffac4ca8c66c3d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-02 16:29:20 +0530

    WarmUp

--- 2)WarmUp/StarPattern.js
+++ 2)WarmUp/StarPattern.js
@@ -151,6 +151,7 @@ for (let i = 0; i < t; i++) {
   let toggle = 1;
   for (let j = 0; j < i + 1; j++) {
     row = row + toggle;
+    // Switch the toggle
     if (toggle === 1) {
       toggle = 0;
     } else {
@@ -174,6 +175,7 @@ for (let i = 0; i < n; i++) {
   let row = "";
   for (let j = 0; j < i + 1; j++) {
     row = row + toggle;
+    // Switch the toggle
     if (toggle === 1) {
       toggle = 0;
     } else {
