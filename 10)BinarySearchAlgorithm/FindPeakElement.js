6e89db20c8ac026f6ec1d914bf36d6872ec56dc8
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-03 14:18:09 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/FindPeakElement.js
+++ 10)BinarySearchAlgorithm/FindPeakElement.js
@@ -32,6 +32,6 @@ var findPeakElement2 = function (arr) {
   return r; // return l also works
 };
 
-let arr2 = [1, 2, 3, 1];
+let arr2 = [1, 2, 1, 3, 5, 6, 4];
 let res2 = findPeakElement2(arr2);
 console.log(res2);
