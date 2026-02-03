aff8fe1214ed4a882ac436d68f5b1959a4a7d99a
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-03 07:03:11 +0530

    Arrays

--- 4)Arrays/MissingNumber.js
+++ 4)Arrays/MissingNumber.js
@@ -1,9 +1,9 @@
 // Missing Number (LC:268)
 
-// Optimized Solution (TC:O(n), SC:O(1))
-var missingNumber = function (nums) {
+// (Way-01) Optimized Solution (TC:O(n), SC:O(1))
+var missingNumber1 = function (nums) {
   let n = nums.length;
-  let totalSum = Math.floor(n * (n + 1) / 2);
+  let totalSum = Math.floor((n * (n + 1)) / 2);
   let partialSum = 0;
   for (let i = 0; i < n; i++) {
     partialSum += nums[i];
@@ -11,6 +11,19 @@ var missingNumber = function (nums) {
   return totalSum - partialSum;
 };
 
-let nums = [3, 0, 1];
-let res = missingNumber(nums);
+let nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+let res = missingNumber1(nums);
 console.log(res);
+
+// (Way-02) Optimized Solution using XOR (TC:O(n), SC:O(1))
+var missingNumber2 = function (nums) {
+  let xor = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ i ^ nums[i];
+  }
+  return xor;
+};
+
+let arr = [3, 0, 1];
+let res2 = missingNumber2(arr);
+console.log(res2);
