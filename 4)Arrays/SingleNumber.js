eff44d20b41ece2a4e1d87d9546ac8e29331e144
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-03 07:18:39 +0530

    Arrays

--- 4)Arrays/SingleNumber.js
+++ 4)Arrays/SingleNumber.js
@@ -9,7 +9,7 @@ var singleNumber1 = function (nums) {
   return ans;
 };
 
-let nums1 = [2, 2, 1, 3, 3, 4, 4];
+let nums1 = [2, 2, 1];
 let res1 = singleNumber1(nums1);
 console.log(res1);
 
@@ -30,6 +30,6 @@ var singleNumber2 = function (nums) {
   }
 };
 
-let nums2 = [2, 2, 1, 1, 3, 3, 4, 4, 5];
+let nums2 = [4, 1, 2, 1, 2];
 let res2 = singleNumber2(nums2);
 console.log(res2);
