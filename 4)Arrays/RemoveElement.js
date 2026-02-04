96cf6307177a2f6281d9724bdffe7852218df32c
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-04 09:47:46 +0530

    Arrays

--- 4)Arrays/RemoveElement.js
+++ 4)Arrays/RemoveElement.js
@@ -16,4 +16,19 @@ let nums = [0, 1, 2, 2, 3, 0, 4, 2];
 let val = 2;
 let res = removeElement(nums, val);
 console.log(res);
-console.log(nums);
+
+// (TC:O(n), SC:O(1))
+var removeElement2 = function (nums, val) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      if (x != i) {
+        nums[x] = nums[i];
+      }
+      x++;
+    }
+  }
+  return x;
+};
+
+console.log(removeElement2([3, 2, 2, 3], 3));
