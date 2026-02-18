17d7e0b6365bec6a6efe5012a8dcf270c3c61589
rupeshkommindala <rupeshkommindala@gmail.com>
2026-02-18 20:19:46 +0530

    LinkedList

--- 7)LinkedList/DesignLinkedList.js
+++ 7)LinkedList/DesignLinkedList.js
@@ -11,21 +11,6 @@ var MyLinkedList = function () {
 };
 
 /**
- * @param {number} index
- * @return {number}
- */
-MyLinkedList.prototype.get = function (index) {
-  if (index < 0 || index >= this.size) {
-    return -1;
-  }
-  let curr = this.head;
-  for (let i = 0; i < index; i++) {
-    curr = curr.next;
-  }
-  return curr.val;
-};
-
-/**
  * @param {number} val
  * @return {void}
  */
