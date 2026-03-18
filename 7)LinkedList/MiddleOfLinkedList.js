fcb13369075e86a5f4c6d77741a3e1c3433c2d56
rupeshkommindala <rupeshkommindala@gmail.com>
2026-03-18 21:27:37 +0530

    LinkedList

--- 7)LinkedList/MiddleOfLinkedList.js
+++ 7)LinkedList/MiddleOfLinkedList.js
@@ -1,30 +1,9 @@
 // Middle of the Linked List (LC:876)
 
-// (Way-1) Optimized Solution (TC:O(n), SC:O(1))
+// Optimized Solution (TC:O(n), SC:O(1))
 var middleNode = function (head) {
   let slow = head;
   let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  return slow;
-};
-
-// (Way-2) Optimized Solution (TC:O(n), SC:O(1))
-var middleNode = function (head) {
-  let slow = head;
-  let fast = head;
-  while (fast !== null && fast.next !== null) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  return slow;
-};
-
-// (Way-3) Optimized Solution (TC:O(n), SC:O(1))
-var middleNode = function (head) {
-  let slow = (fast = head);
   while (fast !== null && fast.next !== null) {
     slow = slow.next;
     fast = fast.next.next;
