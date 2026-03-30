4b1397174e15ca16e4bf395d1b3900ab52723898
rupeshkommindala <rupeshkommindala@gmail.com>
2026-03-30 22:08:55 +0530

    BinaryTree

---
+++ 12)BinaryTree/LevelOrderTraversalQueueBFS.js
@@ -0,0 +1,17 @@
+var levelOrder = function(root) {
+    if(!root) return [];
+    let q = [];
+    let curr = root;
+
+    while(curr || stack.length) {
+        while(curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        ans.push(curr.val);
+        curr = curr.right;
+    }
+    return ans;
+};
+   
