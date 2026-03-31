aa313d70572d040b1be99f8606bd02cdae37d80d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-03-31 21:27:53 +0530

    BinaryTree

--- 12)BinaryTree/MaximumDepthOfBinaryTree.js
+++ 12)BinaryTree/MaximumDepthOfBinaryTree.js
@@ -1,11 +1,11 @@
-var maxDepth = function(root) {
-    if(!root) return 0;
-    let maxDepth = 0;
-    let traversal = (curr, depth) => {
-        maxDepth = Math.max(maxDepth, depth);
-        curr.left && traversal(curr.left, depth + 1);
-        curr.right && traversal(curr.right, depth + 1);
-    }
-    traversal(root, 1);
-    return maxDepth;
-};       
+var maxDepth = function (root) {
+  if (!root) return 0;
+  let maxDepth = 0;
+  let traversal = (curr, depth) => {
+    maxDepth = Math.max(maxDepth, depth);
+    curr.left && traversal(curr.left, depth + 1);
+    curr.right && traversal(curr.right, depth + 1);
+  };
+  traversal(root, 1);
+  return maxDepth;
+};
