476f6dc4685c4c9eebf44ddf95db8a1ba7b08770
rupeshkommindala <rupeshkommindala@gmail.com>
2026-04-01 21:23:31 +0530

    BinaryTree

--- 12)BinaryTree/PathSum.js
+++ 12)BinaryTree/PathSum.js
@@ -1,18 +1,16 @@
-
-var hasPathSum = function(root, targetSum) {
-    if(!root) return false;
-    let ans = false;
-    let traverse = (curr, currSum) => {
-        let newSum = currSum + curr.val;
-        if(!curr.left && !curr.right) {
-            if(newSum === targetSum) {
-                ans = ans || true;
-            }
-        }
-        curr.left && traverse(curr.left, newSum);
-        curr.right && traverse(curr.right, newSum);
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+  let ans = false;
+  let traverse = (curr, currSum) => {
+    let newSum = currSum + curr.val;
+    if (!curr.left && !curr.right) {
+      if (newSum === targetSum) {
+        ans = ans || true;
+      }
     }
-    traverse(root, 0);
-    return ans;
-}; 
-    
+    curr.left && traverse(curr.left, newSum);
+    curr.right && traverse(curr.right, newSum);
+  };
+  traverse(root, 0);
+  return ans;
+};
