3ed6728f31cba77953c48a7a2690134f557ede20
rupeshkommindala <rupeshkommindala@gmail.com>
2026-04-08 20:43:20 +0530

    LinkedList

--- 7)LinkedList/IntersectionOfTwoLL.js
+++ 7)LinkedList/IntersectionOfTwoLL.js
@@ -2,21 +2,21 @@
 
 // Brute Force Solution (TC:O(m*n), SC:O(1))
 var getIntersectionNode = function (headA, headB) {
-  let curr = headA;
-  while (curr) {
+  let curr1 = headA;
+  while (curr1) {
     let curr2 = headB;
     while (curr2) {
-      if (curr === curr2) {
-        return curr;
+      if (curr1 === curr2) {
+        return curr1;
       }
       curr2 = curr2.next;
     }
-    curr = curr.next;
+    curr1 = curr1.next;
   }
   return null;
 };
 
-// Optimized Solution (TC:O(m+n), SC:O(n))
+// Optimized Solution (TC:O(n+m), SC:O(n))
 var getIntersectionNode = function (headA, headB) {
   let store = new Set();
   while (headB) {
