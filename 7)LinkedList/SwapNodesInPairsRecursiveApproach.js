9634646279506ede47c97a22bf00f766899dd30a
rupeshkommindala <rupeshkommindala@gmail.com>
2026-04-20 21:24:52 +0530

    LinkedList

--- 7)LinkedList/SwapNodesInPairsRecursiveApproach.js
+++ 7)LinkedList/SwapNodesInPairsRecursiveApproach.js
@@ -1,11 +1,10 @@
-var swapPairs = function(head) {
-    if (!head || !head.next) return head;
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
 
-    let l = head;
-    let r = head.next;
+  let l = head;
+  let r = head.next;
 
-    l.next = swapPairs(r.next);
-    r.next = l;
-    return r;
+  l.next = swapPairs(r.next);
+  r.next = l;
+  return r;
 };
-    
