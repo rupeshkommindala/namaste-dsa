180692d6bbc7c09f030d96db53308a3a534be633
rupeshkommindala <rupeshkommindala@gmail.com>
2026-04-25 22:07:29 +0530

    SearchingSorting-Advanced

--- 21)SearchingSorting(Advanced)/BucketSort.js
+++ 21)SearchingSorting(Advanced)/BucketSort.js
@@ -1,25 +1,24 @@
 function bucketSort(arr) {
-    let n = arr.length;
-    
-    let buckets = Array.from({ length: n}, () => []);
-    
-    for(let x of arr){
-        let index = Math.floor(x * n)
-        buckets[index].push(x);
+  let n = arr.length;
+
+  let buckets = Array.from({ length: n }, () => []);
+
+  for (let x of arr) {
+    let index = Math.floor(x * n);
+    buckets[index].push(x);
+  }
+
+  for (let bucket of buckets) {
+    bucket.sort((a, b) => a - b);
+  }
+  let i = 0;
+  for (let bucket of buckets) {
+    for (let num of bucket) {
+      arr[i++] = num;
     }
-    
-    for(let bucket of buckets){
-        bucket.sort((a, b) => a - b);
-    }
-    let i = 0;
-    for(let bucket of buckets) {
-        for(let num of bucket) {
-            arr[i++] = num;
-        }
-    }
-    return arr;
+  }
+  return arr;
 }
 
- 
-const nums = [0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23, 0.68]
+const nums = [0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23, 0.68];
 console.log(bucketSort(nums));
