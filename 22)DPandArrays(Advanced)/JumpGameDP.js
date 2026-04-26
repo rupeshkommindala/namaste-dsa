54a583f7cb29b966bab411517349d3a549854457
rupeshkommindala <rupeshkommindala@gmail.com>
2026-04-26 20:37:14 +0530

    DPandArrays-Advanced

---
+++ 22)DPandArrays(Advanced)/JumpGameDP.js
@@ -0,0 +1,23 @@
+var canJump = function(nums) {
+    let end = nums.length - 1;
+    let dp = new Array(nums.length).fill(-1);
+
+    let dfs = (start) => {
+        if(start === end) return true;
+
+        if(dp[start] != -1) return dp[start];
+
+        let ans = false;
+
+        for(let i = 1; i <= nums[start] && !ans; i++){
+            if(start + i <= end){
+                ans = ans || dfs(start + i);
+            }
+        }
+
+        dp[start] = ans;
+        return ans;
+    }
+
+    return dfs(0);
+};
