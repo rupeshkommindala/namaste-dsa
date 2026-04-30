a5b93f7d234934938d2817ab97868b3d0bcf31fa
rupeshkommindala <rupeshkommindala@gmail.com>
2026-04-30 19:59:49 +0530

    DPandArrays-Advanced

---
+++ 22)DPandArrays(Advanced)/SuperEggDrop.js
@@ -0,0 +1,12 @@
+var superEggDrop = function(k, n) {
+    let dp = new Array(k + 1).fill(0);
+    let moves = 0;
+
+    while(dp[k] < n){
+        moves++;
+        for(let i = k; i >= 1; i--) {
+            dp[i] = 1 + dp[i] + dp[i-1];
+        }
+    }
+    return moves;
+};
