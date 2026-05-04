f214463925e9dca6dd859ac6f7b9f21fa1622721
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-04 21:10:34 +0530

    Graphs

--- 19)Graphs/DFSRecursive.js
+++ 19)Graphs/DFSRecursive.js
@@ -1,26 +1,26 @@
-var validPath = function(n, edges, source, destination) {
-    let map = {};
-    for(let [x, y] of edges) {
-        if(!map[x]) map[x] = [];
-        if(!map[y]) map[y] = [];
-        map[x].push(y);
-        map[y].push(x);
+var validPath = function (n, edges, source, destination) {
+  let map = {};
+  for (let [x, y] of edges) {
+    if (!map[x]) map[x] = [];
+    if (!map[y]) map[y] = [];
+    map[x].push(y);
+    map[y].push(x);
+  }
+  let q = [source];
+  let visited = new Set();
+  let dfs = (curr) => {
+    if (curr === destination) {
+      return true;
     }
-    let q = [source];
-    let visited = new Set();
-    let dfs = (curr) => {
-        if(curr === destination){
-            return true;
+    visited.add(curr);
+    for (let neighbor of map[curr]) {
+      if (!visited.has(neighbor)) {
+        if (dfs(neighbor)) {
+          return true;
         }
-        visited.add(curr);
-        for(let neighbor of map[curr]) {
-            if(!visited.has(neighbor)) {
-                if(dfs(neighbor)){
-                    return true;
-                }
-            }
-        }
+      }
+    }
     return false;
-    }
-    return dfs(source);
+  };
+  return dfs(source);
 };
