5f03920b22aa2298a0eac4e664f13f3605ee5754
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-05 20:25:22 +0530

    Graphs

---
+++ 19)Graphs/ReconstructItinerary.js
@@ -0,0 +1,23 @@
+var findItinerary = function(tickets) {
+  let graph = {};
+  for (let [from, to] of tickets) {
+    if (!graph[from]) graph[from] = [];
+    graph[from].push(to);
+  }
+  for (let node in graph) {
+    graph[node].sort(); // ascending lexicographic
+  }
+
+  let path = [];
+  let dfs = (curr) => {
+    while (graph[curr] && graph[curr].length) {
+      let neighbor = graph[curr].shift(); // remove from front
+      dfs(neighbor);
+    }
+    path.push(curr);
+  };
+
+  dfs("JFK");
+  return path.reverse(); // reverse before returning
+};
+  
