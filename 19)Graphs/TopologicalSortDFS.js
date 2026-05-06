b8261ee2426167a16217f1d8983c7e0a2857ec45
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-06 21:29:54 +0530

    Graphs

---
+++ 19)Graphs/TopologicalSortDFS.js
@@ -0,0 +1,36 @@
+function topologicalSortDFS(n, graph) {
+    let ans = [];
+    let visited = new Set();
+
+    function dfs(curr) {
+        visited.add(curr);
+
+        for (let neighbor of graph[curr]) {
+            if (!visited.has(neighbor)) {
+                dfs(neighbor);
+            }
+        }
+
+        ans.push(curr);
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (!visited.has(i)) {
+            dfs(i);
+        }
+    }
+
+    return ans.reverse();
+}
+
+const n = 6;
+const adj = [
+    [],        // 0
+    [],        // 1
+    [3],       // 2 -> 3
+    [1],       // 3 -> 1
+    [0, 1],    // 4 -> 0, 1
+    [0, 2]     // 5 -> 0, 2
+];
+
+console.log(topologicalSortDFS(n, adj));
