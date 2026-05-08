83cfec0f37fc945f04c9fa8f04eca2e15da77724
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-08 20:54:08 +0530

    GreedyAlgorithm

--- 17)GreedyAlgorithm/TwoCityScheduling.js
+++ 17)GreedyAlgorithm/TwoCityScheduling.js
@@ -1,14 +1,14 @@
 var twoCitySchedCost = function (costs) {
-        costs.sort((a, b) => (b[1] - b[0]) - (a[1] - a[0]));
-        let ans = 0;
-        let n = costs.length / 2;
+  costs.sort((a, b) => b[1] - b[0] - (a[1] - a[0]));
+  let ans = 0;
+  let n = costs.length / 2;
 
-        for (let i = 0; i < n; i++) {
-            ans += costs[i][0];
-        }
-        for (let i = n; i < 2 * n; i++) {
-            ans += costs[i][1];
-        }
+  for (let i = 0; i < n; i++) {
+    ans += costs[i][0];
+  }
+  for (let i = n; i < 2 * n; i++) {
+    ans += costs[i][1];
+  }
 
-        return ans;
-        };
+  return ans;
+};
