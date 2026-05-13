feb725d3b8b5ff6fc29260d4491f0ad763927a2d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-13 21:05:54 +0530

    DP

--- 18)DynamicProgramming/HouseRobber.js
+++ 18)DynamicProgramming/HouseRobber.js
@@ -1,15 +1,14 @@
-var rob = function(val) { 
-    let n = val.length; 
-    if(n == 1) 
-    return val[0]; 
-    
-    let p2 = p1 = 0; 
-    for(let i=0; i < n; i++){ 
-        curr = Math.max(val[i] + p2, p1); 
-        let temp = p1; 
-        p1 = curr; 
-        p2 = temp; 
-        curr++; 
-     } 
-    return p1; 
+var rob = function (val) {
+  let n = val.length;
+  if (n == 1) return val[0];
+
+  let p2 = (p1 = 0);
+  for (let i = 0; i < n; i++) {
+    curr = Math.max(val[i] + p2, p1);
+    let temp = p1;
+    p1 = curr;
+    p2 = temp;
+    curr++;
+  }
+  return p1;
 };
