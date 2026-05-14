147f7e3e6f2dd26f1c7ff891169212f2f171e353
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-14 21:00:21 +0530

    Strings(Advanced)

--- 23)Strings(Advanced)/DecodeString.js
+++ 23)Strings(Advanced)/DecodeString.js
@@ -1,35 +1,32 @@
-var decodeString = function(s) {
-    let i = 0;
+var decodeString = function (s) {
+  let i = 0;
 
-    let decode = () => {
-        let res = "";
-        let num = 0;
+  let decode = () => {
+    let res = "";
+    let num = 0;
 
-        while (i < s.length) {
-            let char = s[i];
+    while (i < s.length) {
+      let char = s[i];
 
-            if (char >= '0' && char <= '9') {
-                num = num * 10 + parseInt(char);
-                i++;
-            } 
-            else if (char === "[") {
-                i++;
-                let str = decode();
-                res += str.repeat(num);
-                num = 0;
-            } 
-            else if (char === "]") {
-                i++;
-                return res;
-            } 
-            else {
-                res += char;
-                i++;
-            }
-        }
-
+      if (char >= "0" && char <= "9") {
+        num = num * 10 + parseInt(char);
+        i++;
+      } else if (char === "[") {
+        i++;
+        let str = decode();
+        res += str.repeat(num);
+        num = 0;
+      } else if (char === "]") {
+        i++;
         return res;
-    };
+      } else {
+        res += char;
+        i++;
+      }
+    }
 
-    return decode();
+    return res;
+  };
+
+  return decode();
 };
