88215805bc85134f4a60e28d17c9f25c44a7a936
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-15 21:06:08 +0530

    GreedyAlgorithm

---
+++ 17)GreedyAlgorithm/TaskScheduler.js
@@ -0,0 +1,18 @@
+var leastInterval = function(tasks, n) {
+    let freq = Array(26).fill(0);
+    let maxFreq = 0;
+
+    for (let i = 0; i < tasks.length; i++) {
+        let curr = tasks[i].charCodeAt() - 65;  // Map 'A'-'Z' to 0-25
+        freq[curr]++;
+        maxFreq = Math.max(maxFreq, freq[curr]);
+    }
+    let countMaxFreq = 0;
+    for (let i = 0; i < 26; i++) {
+        if (freq[i] === maxFreq) {
+            countMaxFreq++;
+        }
+    }
+    let cycles = (maxFreq - 1) * (n + 1) + countMaxFreq;
+    return Math.max(tasks.length, cycles);
+};
