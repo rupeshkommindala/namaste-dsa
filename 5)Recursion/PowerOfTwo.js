8c697805bc0ad8c6629b3bd1bd257877759336a3
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-16 17:31:36 +0530

    Recursion

--- 5)Recursion/PowerOfTwo.js
+++ 5)Recursion/PowerOfTwo.js
@@ -4,7 +4,7 @@
 var isPowerOfTwo = function (n) {
   if (n === 1) {
     return true;
-  } else if (n % 2 === 1 || n < 1) {
+  } else if (n < 1 || n % 2 === 1) {
     return false;
   }
   return isPowerOfTwo(n / 2);
