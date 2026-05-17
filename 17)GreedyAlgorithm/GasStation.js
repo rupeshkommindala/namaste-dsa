30c502a3adb16d282086c44b6573414085288c61
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-17 20:40:09 +0530

    GreedyAlgorithm

--- 17)GreedyAlgorithm/GasStation.js
+++ 17)GreedyAlgorithm/GasStation.js
@@ -1,16 +1,16 @@
 var canCompleteCircuit = function (gas, cost) {
-    let currGain = 0;
-    let totalGain = 0;
-    let ans = 0;
-    for (let i = 0; i < gas.length; i++) {
-        let gain = gas[i] - cost[i];
-        currGain += gain;
-        totalGain += gain;
+  let currGain = 0;
+  let totalGain = 0;
+  let ans = 0;
+  for (let i = 0; i < gas.length; i++) {
+    let gain = gas[i] - cost[i];
+    currGain += gain;
+    totalGain += gain;
 
-        if (currGain < 0) {
-            ans = i + 1;
-            currGain = 0;
-        }
+    if (currGain < 0) {
+      ans = i + 1;
+      currGain = 0;
     }
-    return totalGain < 0 ? -1 : ans;
-}
+  }
+  return totalGain < 0 ? -1 : ans;
+};
