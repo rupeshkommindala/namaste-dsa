2597b98abcdd838c176f92d0528645131ef00774
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-18 20:54:50 +0530

    GreedyAlgorithm

---
+++ 17)GreedyAlgorithm/CarPooling.js
@@ -0,0 +1,16 @@
+var carPooling = function(trips, carCapacity) {
+    let loc = Array(1001).fill(0);
+    for (let i = 0; i < trips.length; i++) {
+        let [passengers, from, to] = trips[i];
+        loc[from] += passengers;   // Pick up passengers
+        loc[to] -= passengers;     // Drop off passengers
+    }
+    let usedCapacity = 0;
+    for (let i = 0; i < 1001; i++) {
+        usedCapacity += loc[i];
+        if (usedCapacity > carCapacity) {
+            return false; 
+        }
+    }
+    return true; 
+};
