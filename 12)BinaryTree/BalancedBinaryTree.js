a91a6de88c7207dc6253de7964e84bc58573f5a0
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-19 21:40:37 +0530

    BinaryTree

--- 12)BinaryTree/BalancedBinaryTree.js
+++ 12)BinaryTree/BalancedBinaryTree.js
@@ -1,16 +1,14 @@
-
-var isBalanced = function(root) {
-    let ans = true;
-    let calculateHeight = (curr) => {
-        if(!curr) return 0;
-        let leftHeight = calculateHeight(curr.left);
-        let rightHeight = calculateHeight(curr.right);
-        if(Math.abs(leftHeight - rightHeight) > 1){
-            ans = ans && false;
-        }
-        return 1 + Math.max(leftHeight, rightHeight);
+var isBalanced = function (root) {
+  let ans = true;
+  let calculateHeight = (curr) => {
+    if (!curr) return 0;
+    let leftHeight = calculateHeight(curr.left);
+    let rightHeight = calculateHeight(curr.right);
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      ans = ans && false;
     }
-    calculateHeight(root);
-    return ans;
+    return 1 + Math.max(leftHeight, rightHeight);
+  };
+  calculateHeight(root);
+  return ans;
 };
-   
