99b513178956bd54e4d4d78971a5fceb26412220
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-22 20:58:35 +0530

    BinarySearchTree

--- 13)BinarySearchTree/KthSmallestElement.js
+++ 13)BinarySearchTree/KthSmallestElement.js
@@ -1,15 +1,15 @@
-var kthSmallest = function(root, k) {
-    let ans = null;
-    let count = k;
-    let traversal = (curr) => {
-        if(ans != null) return;
-        curr.left && traversal(curr.left);
-        --count;
-        if(count == 0){
-            ans = curr.val;
-        }
-        curr.right && traversal(curr.right);
+var kthSmallest = function (root, k) {
+  let ans = null;
+  let count = k;
+  let traversal = (curr) => {
+    if (ans != null) return;
+    curr.left && traversal(curr.left);
+    --count;
+    if (count == 0) {
+      ans = curr.val;
     }
-    traversal(root);
-    return ans;
+    curr.right && traversal(curr.right);
+  };
+  traversal(root);
+  return ans;
 };
