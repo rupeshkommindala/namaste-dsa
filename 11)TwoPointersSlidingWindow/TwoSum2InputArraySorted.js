48a8cb94bf5d2695cf5ce27e042f461fe1f0d19a
rupeshkommindala <rupeshkommindala@gmail.com>
2026-05-29 19:33:24 +0530

    TwoPointersSlidingWindow

--- 11)TwoPointersSlidingWindow/TwoSum2InputArraySorted.js
+++ 11)TwoPointersSlidingWindow/TwoSum2InputArraySorted.js
@@ -6,10 +6,10 @@ var twoSum = function (arr, target) {
   let j = arr.length - 1;
   while (i < j) {
     let sum = arr[i] + arr[j];
-    if (sum > target) {
-      j--;
-    } else if (sum < target) {
+    if (sum < target) {
       i++;
+    } else if (sum > target) {
+      j--;
     } else {
       return [i + 1, j + 1];
     }
@@ -17,6 +17,6 @@ var twoSum = function (arr, target) {
 };
 
 let nums = [2, 7, 11, 15];
-let target = 26;
+let target = 9;
 let res = twoSum(nums, target);
 console.log(res);
