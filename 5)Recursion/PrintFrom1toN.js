7c1d2395b4158d2d08f6c5d8d7d1db4c41865c22
rupeshkommindala <rupeshkommindala@gmail.com>
2026-06-26 17:14:53 +0530

    Recursion

--- 5)Recursion/PrintFrom1toN.js
+++ 5)Recursion/PrintFrom1toN.js
@@ -1,12 +1,11 @@
 // Print from 1 to N using Recursion
 
 // (TC:O(n), SC:O(n) Recursion stack)
-let n = 10;
+let n = 5;
 function printFrom1toN(x) {
   if (x > n) {
     return;
   }
-
   console.log(x);
   x++;
   printFrom1toN(x);
