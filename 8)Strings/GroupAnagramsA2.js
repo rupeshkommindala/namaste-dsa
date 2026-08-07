52183037e9e9e462232f2c2365fde95f147c508d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-08-07 20:26:15 +0530

    Strings

--- 8)Strings/GroupAnagramsA2.js
+++ 8)Strings/GroupAnagramsA2.js
@@ -1,1 +1,24 @@
+// Group Anagrams (LC:49)
 
+// Optimized Solution (TC:O(n*m), SC:O(n*m))
+var groupAnagrams = function (strs) {
+  let map = {};
+  for (let i = 0; i < strs.length; i++) {
+    let freqArr = Array(26).fill(0);
+    let s = strs[i];
+    for (let j = 0; j < s.length; j++) {
+      let index = s[j].charCodeAt() - "a".charCodeAt();
+      freqArr[index]++;
+    }
+    let key = "";
+    for (let k = 0; k < 26; k++) {
+      key = key + String.fromCharCode(k) + freqArr[k];
+    }
+    if (!map[key]) {
+      map[key] = [s];
+    } else {
+      map[key].push(s);
+    }
+  }
+  return [...Object.values(map)];
+};
