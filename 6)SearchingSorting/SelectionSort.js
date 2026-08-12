59bd42efa7abbc58f56e6419b7dfdb172af0fd16
rupeshkommindala <rupeshkommindala@gmail.com>
2026-08-12 21:47:16 +0530

    Sorting

--- 6)SearchingSorting/SelectionSort.js
+++ 6)SearchingSorting/SelectionSort.js
@@ -1,1 +1,23 @@
+// Selection sort
 
+// (TC:O(n^2), SC:O(1))
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
+let arr = [2, 1, 5, 3, 4];
+let res = selectionSort(arr);
+console.log(res);
