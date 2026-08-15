a84db5d661292f72d8210bbd93666549b5a8d7b6
rupeshkommindala <rupeshkommindala@gmail.com>
2026-08-15 20:19:39 +0530

    Hashing

--- 15)Hashing/IsomorphicStrings.js
+++ 15)Hashing/IsomorphicStrings.js
@@ -1,1 +1,17 @@
 
+ var isIsomorphic = function(s, t) {
+      let mapSToT = {};
+      let mapTToS = {};
+  
+      for (let i = 0; i < s.length; i++) {
+          if (!mapSToT[s[i]] && !mapTToS[t[i]]) {
+              mapSToT[s[i]] = t[i];
+              mapTToS[t[i]] = s[i];
+          } else if (mapTToS[t[i]] !== s[i] || mapSToT[s[i]] !== t[i]) {
+              return false;
+          }
+      }
+  
+      return true;
+  };
+    
