05f0b257b90cc815d5a057e3fabdc0684efb12ac
rupeshkommindala <rupeshkommindala@gmail.com>
2026-08-21 15:03:02 +0530

    TwoPointersSlidingWindow

--- 11)TwoPointersSlidingWindow/ContainerWithMostWater.js
+++ 11)TwoPointersSlidingWindow/ContainerWithMostWater.js
@@ -1,18 +1,38 @@
 // Container With Most Water (LC:11)
 
-function maxArea(arr) {
+// Brute Force Solution (TC:O(n^2), SC:O(1)) (Time Limit Exceeded)
+var maxArea1 = function (arr) {
+  let maxWater = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let area = Math.min(arr[i], arr[j]) * (j - i);
+      maxWater = Math.max(area, maxWater);
+    }
+  }
+  return maxWater;
+};
+
+let area1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+let res1 = maxArea1(area1);
+console.log(res1);
+
+// Optimized Solution (TC:O(n), SC:O(1))
+var maxArea2 = function (arr) {
   let i = 0;
   let j = arr.length - 1;
   let maxWater = 0;
   while (i < j) {
     let area = Math.min(arr[i], arr[j]) * (j - i);
     maxWater = Math.max(area, maxWater);
-    if (arr[i] < arr[j]) i++;
-    else j--;
+    if (arr[i] < arr[j]) {
+      i++;
+    } else {
+      j--;
+    }
   }
   return maxWater;
-}
-let arr = [1, 1];
-let res = maxArea(arr);
-console.log(res);
+};
 
+let area2 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+let res2 = maxArea2(area2);
+console.log(res2);
