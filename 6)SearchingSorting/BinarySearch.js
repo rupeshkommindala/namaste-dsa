8f092aa015c8cb2e0834db2965dac8c15d84c12c
rupeshkommindala <rupeshkommindala@gmail.com>
2026-08-23 20:33:09 +0530

    Searching

--- 6)SearchingSorting/BinarySearch.js
+++ 6)SearchingSorting/BinarySearch.js
@@ -17,6 +17,6 @@ var search = function (arr, target) {
   return -1;
 };
 
-let arr = [-1, 0, 3, 5, 9, 12];
-let result = search(arr, 9);
+let nums = [-1, 0, 3, 5, 9, 12];
+let result = search(nums, 9);
 console.log(result);
