4a134e41324d48eea67dcb7226c0aa43055607dc
rupeshkommindala <rupeshkommindala@gmail.com>
2026-08-25 21:33:27 +0530

    Sorting

--- 6)SearchingSorting/BubbleSort.js
+++ 6)SearchingSorting/BubbleSort.js
@@ -5,7 +5,7 @@ function bubbleSort(arr) {
   let n = arr.length;
   for (let i = 0; i < n - 1; i++) {
     let isSwapped = false;
-    for (let j = 0; j < n - 1 - i; j++) {
+    for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -13,13 +13,10 @@ function bubbleSort(arr) {
         isSwapped = true;
       }
     }
-    if (!isSwapped) {
-      break;
-    }
+    if (!isSwapped) break;
   }
   return arr;
 }
-
-let arr = [5, 2, 4, 1];
+let arr = [2, 5, 4, 1];
 let res = bubbleSort(arr);
 console.log(res);
