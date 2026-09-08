576ce056986572384407a839010ed34ead78046d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-09-08 21:33:22 +0530

    DynamicProgramming

---
+++ 18)DynamicProgramming/MaximumSubArrayKadanesAlgorithm.js
@@ -0,0 +1,11 @@
+function maxSubArray(arr) {
+  let currSum = arr[0];
+  let maxSum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    currSum = Math.max(currSum + arr[i], arr[i]);
+    maxSum = Math.max(currSum, maxSum);
+  }
+
+  return maxSum;
+}
