6e8540b5ba28ab9d1712f4b45a7505a7ecb8e461
rupeshkommindala <rupeshkommindala@gmail.com>
2026-09-09 21:43:07 +0530

    DynamicProgramming

---
+++ 18)DynamicProgramming/CoinChangeBottomUpRecursive.js
@@ -0,0 +1,15 @@
+var coinChange = function (coins, amount) {
+  let n = coins.length;
+  let dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let rem = 1; rem <= amount; rem++) {
+    for (let j = 0; j < n; j++) {
+      let remainingAmount = rem - coins[j];
+      if (remainingAmount >= 0) {
+        dp[rem] = Math.min(dp[rem], 1 + dp[remainingAmount]);
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
