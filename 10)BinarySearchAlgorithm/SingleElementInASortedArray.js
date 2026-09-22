bdcce2ac521f104ec00f6ad9ba83b220bbbafa9d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-09-22 12:11:47 +0530

    BinarySearchAlgorithm

--- 10)BinarySearchAlgorithm/SingleElementInASortedArray.js
+++ 10)BinarySearchAlgorithm/SingleElementInASortedArray.js
@@ -17,7 +17,7 @@ var singleNonDuplicate1 = function (nums) {
   }
 };
 
-let nums1 = [3, 3, 7, 7, 10, 11, 11];
+let nums1 = [1, 1, 2, 3, 3, 4, 4, 8, 8];
 let res1 = singleNonDuplicate1(nums1);
 console.log(res1);
 
@@ -30,7 +30,7 @@ var singleNonDuplicate2 = function (nums) {
   return ans;
 };
 
-let nums2 = [3, 3, 7, 7, 10, 11, 11];
+let nums2 = [1, 1, 2, 3, 3, 4, 4, 8, 8];
 let res2 = singleNonDuplicate2(nums2);
 console.log(res2);
 
@@ -52,7 +52,7 @@ var singleNonDuplicate3 = function (nums) {
   return nums[r]; // return nums[l] also works
 };
 
-let nums3 = [3, 3, 7, 7, 10, 11, 11];
+let nums3 = [1, 1, 2, 3, 3, 4, 4, 8, 8];
 let res3 = singleNonDuplicate3(nums3);
 console.log(res3);
 
@@ -62,6 +62,7 @@ var singleNonDuplicate4 = function (nums) {
   let r = nums.length - 1;
   while (l <= r) {
     let m = l + Math.floor((r - l) / 2);
+    // Pair is on left
     if (nums[m] === nums[m - 1]) {
       let leftCount = m - 1 - l;
       if (leftCount % 2 === 1) {
@@ -69,6 +70,7 @@ var singleNonDuplicate4 = function (nums) {
       } else {
         l = m + 1;
       }
+      // Pair is on right
     } else if (nums[m] === nums[m + 1]) {
       let leftCount = m - l;
       if (leftCount % 2 === 1) {
@@ -76,12 +78,13 @@ var singleNonDuplicate4 = function (nums) {
       } else {
         l = m + 2;
       }
+      // Single Element
     } else {
       return nums[m];
     }
   }
 };
 
-let nums4 = [3, 3, 7, 7, 10, 11, 11];
+let nums4 = [1, 1, 2, 3, 3, 4, 4, 8, 8];
 let res4 = singleNonDuplicate4(nums4);
 console.log(res4);
