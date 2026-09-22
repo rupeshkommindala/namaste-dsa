56b46bd23a5627dbc4a19f20ee1c7bdaa2959434
rupeshkommindala <rupeshkommindala@gmail.com>
2026-09-22 12:26:28 +0530

    LinkedList

--- 7)LinkedList/ReverseLinkedList.js
+++ 7)LinkedList/ReverseLinkedList.js
@@ -1,5 +1,10 @@
 // Reverse Linked List (LC:206)
 
+/**
+ * Approach: Here we have to keep track of previous
+ * value and also we have to store the next value
+ */
+
 // Optimized Solution (TC:O(n), SC:O(1))
 var reverseList = function (head) {
   let prev = null;
